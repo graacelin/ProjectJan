925174e6803cc6eac9fc9ed0ac047b41df95cbc9
Ryan Tan <65996005+ryn5@users.noreply.github.com>
2023-07-09 04:36:54 -0700

    Add image and music to credits scene

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("GameScene");
+        SceneManager.LoadScene("CreditsScene");
     }
 
     public void ExitGame()
