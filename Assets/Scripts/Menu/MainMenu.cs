712372a33b33d912e96f15fb53b1c726221573ba
Ryan Tan <ryanmtan5@gmail.com>
2023-08-07 01:18:13 -0700

    Add start and exit SFX

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,6 +5,9 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource soundPlayer;
+    public float exitDelay = 0.3f;    
+
     public void StartGame()
     {
         SceneManager.LoadScene("GameScene");
@@ -12,6 +15,11 @@ public class MainMenu : MonoBehaviour
 
     public void ExitGame()
     {
+        soundPlayer.Play();
+        Invoke("Quit", exitDelay);
+    }
+
+    void Quit() {
         Application.Quit();
     }
 }
